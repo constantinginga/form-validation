4f22ded788258d04d6848c688d4c28bd42293cde
Constantin Ginga <costelgn@gmail.com>
2020-12-28 08:00:02 +0100

    Add babel

--- webpack.config.js
+++ webpack.config.js
@@ -51,6 +51,16 @@ module.exports = {
         ],
       },
       { test: /\.png$/, use: 'url-loader?mimetype=image/png' },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', { targets: 'defaults' }]],
+          },
+        },
+      },
     ],
   },
 
